484331fb7c1e2badae5d46047d40131fd855d25a
Xin Bian <xinbian@users.noreply.github.com>
2018-03-30 10:07:27 -0400

    Update MHD_histo.py

--- MHD_histo.py
+++ MHD_histo.py
@@ -9,6 +9,7 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
+import seaborn as sns
 h5file = h5py.File('tests.h5','r+')
 #rng = np.random.RandomState(10)  # deterministic random data
 #a = np.hstack((rng.normal(size=1000),rng.normal(loc=5, scale=2, size=1000)))
@@ -23,7 +24,7 @@ h5file = h5py.File('tests.h5','r+')
 #specify rerun_tag
 istep='117012'
 #'converb0_1','converb0_2','converb0_4','conversion'
-for variable in ['conversion','converb0_1','converb0_2','converb0_4']:
+for variable in ['conversion']:
 	#generate magnetic conversion difference group
     delimiter = ''
 	#write to hdf5 file
@@ -32,19 +33,28 @@ for variable in ['conversion','converb0_1','converb0_2','converb0_4']:
     databk = h5file.get(filepath)
     np_data = np.array(databk)
     np_data = np_data.reshape(np_data.shape[0]*np_data.shape[1]*np_data.shape[2])
-    plt.hist(np_data, bins=500, normed='True',histtype='step', log='True')  
-    plt.xlabel("$\overline {S}_{ij}\overline{B}_i\overline{B}_j$",
+   
+    plt.hist(np_data, bins=2000, normed='True',histtype='step', log='True')  
+    plt.ylim((1E-5, 10))
+    plt.xlim((-70, 70))
+  #  plt.xlabel("$\overline {S}_{ij}\overline{B}_i\overline{B}_j$",
+  #        fontsize=8, color='black')
+    plt.xlabel("$\overline {S}_{ij}\overline{B}_i\overline{B}_j(k=16)-\overline {S}_{ij}\overline{B}_i\overline{B}_j(k=32)$",
           fontsize=8, color='black')
-    plt.ylabel("PDF of "
-          "$\overline {S}_{ij}\overline{B}_i\overline{B}_j$",
-          fontsize=8, color='black')
+   # plt.ylabel("PDF of "
+   #       "$\overline {S}_{ij}\overline{B}_i\overline{B}_j$",
+    #      fontsize=8, color='black')
     fig = plt.gcf()
-    fig.set_size_inches(4, 3)
+    fig.set_size_inches(3, 3)
     plt.tight_layout()
-    titlelist = [variable,'.eps']
+    mean = np.around(np.mean(np_data), decimals=2)
+    plt.axvline(x=np.mean(np_data),color='r')
+    ax = fig.add_subplot(111)
+    ax.annotate('mean\n='+str(mean), xy=(mean, 1), xytext=(mean+20, 0.5),
+            arrowprops=dict(arrowstyle="->"))
+
+    titlelist = ['diff',variable,'.eps']
     title=delimiter.join(titlelist)
     plt.savefig(title, format='eps', dpi=600)
     plt.show()
 h5file.close()
-
-
