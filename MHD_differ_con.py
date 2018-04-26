910bc4063f1360f8c1165083630f98b303353650
Xin Bian <bianxin2006@gmail.com>
2018-04-26 10:00:46 -0400

    update hist and diff

--- MHD_differ_con.py
+++ MHD_differ_con.py
@@ -4,32 +4,36 @@ import numpy as np
 #calculate filtered field#
 #need to specify wavenumber#
 
-nx=ny=nz = input("input the simulation size:")
+h5file = h5py.File('conver.h5','r+')
+h5filetotal = h5py.File('/Volumes/turbDataDrive1/mhd_visual_conversion_1024_abcb0/k=60/conver.h5','r')
 
 #specify rerun_tag
-istep=raw_input("input merge time step (in the form of 000010, for reruntag=10):")
+istep="174000"
 
-for variable in ['converb0_1','converb0_2','converb0_4','conversion']:
-	#generate magnetic conversion difference group
-	delimiter = ''
-	mylist = ['diff/','P', variable]
-	filepath = delimiter.join(mylist)
-	h5file.create_group(filepath)
-	#obtain 32, 16 and take subtraction
-	mylist = ['Fields/','32',variable,'/',istep]
-	filepath = delimiter.join(mylist)  
-	delimiteratabk = h5file.get(filepath)
-	m1 = np.array(databk)
-	#obtain converion_B0
-	mylist = ['Fields/','16',variable,'/',istep]
-	filepath = delimiter.join(mylist)  
-	databk = h5file.get(filepath)
-	m2 = np.array(databk)
-	#calculate difference
-	m1=m1-m2
-	#write to hdf5 file
-	mylist = ['diff/',variable,'/',istep]
-	filepath = delimiter.join(mylist)
-	h5file.create_dataset(filepath,data=m1)
+variable = ['60converion','allconverion']
+#generate magnetic conversion difference group
+delimiter = ''
+mylist = ['diff/','P']
+filepath = delimiter.join(mylist)
+#h5file.create_group(filepath)
+#obtain 32, 16 and take subtraction
+mylist = ['Fields/','P',variable[0],'/',istep]
+filepath = delimiter.join(mylist)  
+databk = h5file.get(filepath)
+m1 = np.array(databk)
+
+
+
+mylist = ['Fields/','P',variable[1],'/',istep]
+filepath = delimiter.join(mylist)  
+databk = h5filetotal.get(filepath)
+m2 = np.array(databk)
+#calculate difference
+m1=m2-m1
+#write to hdf5 file
+mylist = ['diff/','P','/',istep]
+filepath = delimiter.join(mylist)
+h5file.create_dataset(filepath,data=m1)
 
 h5file.close()
+h5filetotal.close()
--- MHD_histo.py
+++ MHD_histo.py
@@ -8,9 +8,7 @@ Created on Wed Dec 21 16:25:53 2016
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy
-import seaborn as sns
-h5file = h5py.File('tests.h5','r+')
+h5file = h5py.File('conver.h5','r+')
 #rng = np.random.RandomState(10)  # deterministic random data
 #a = np.hstack((rng.normal(size=1000),rng.normal(loc=5, scale=2, size=1000)))
 #b=10*np.random.randn(10) 
@@ -21,40 +19,80 @@ h5file = h5py.File('tests.h5','r+')
 #plt.plot(x, norm.pdf(x),'r-', lw=5, alpha=0.6, label='norm pdf')
 
 
+
 #specify rerun_tag
-istep='117012'
-#'converb0_1','converb0_2','converb0_4','conversion'
-for variable in ['conversion']:
-	#generate magnetic conversion difference group
-    delimiter = ''
-	#write to hdf5 file
-    mylist = ['Fields/','Pdiff',variable,'/',istep]
-    filepath = delimiter.join(mylist)
-    databk = h5file.get(filepath)
-    np_data = np.array(databk)
-    np_data = np_data.reshape(np_data.shape[0]*np_data.shape[1]*np_data.shape[2])
-   
-    plt.hist(np_data, bins=2000, normed='True',histtype='step', log='True')  
-    plt.ylim((1E-5, 10))
-    plt.xlim((-70, 70))
-  #  plt.xlabel("$\overline {S}_{ij}\overline{B}_i\overline{B}_j$",
-  #        fontsize=8, color='black')
-    plt.xlabel("$\overline {S}_{ij}\overline{B}_i\overline{B}_j(k=16)-\overline {S}_{ij}\overline{B}_i\overline{B}_j(k=32)$",
-          fontsize=8, color='black')
-   # plt.ylabel("PDF of "
-   #       "$\overline {S}_{ij}\overline{B}_i\overline{B}_j$",
-    #      fontsize=8, color='black')
-    fig = plt.gcf()
-    fig.set_size_inches(3, 3)
+istep='174000'
+norm_const = 1.0/0.2874
+large_label = '$\overline {S}_{ij}\overline{B}_i\overline{B}_j$'
+small_label = '${S}_{ij}{B}_i{B}_j-\overline {S}_{ij}\overline{B}_i\overline{B}_j$'
+
+
+
+def init(istep):
+   delimiter = ''
+   mylist = ['Fields/','P60converion','/',istep]
+   filepath = delimiter.join(mylist)
+   databk = h5file.get(filepath)
+   large_data = np.array(databk)
+
+   mylist = ['diff/','P','/',istep]
+   filepath = delimiter.join(mylist)
+   databk = h5file.get(filepath)
+   small_data = np.array(databk)
+
+   return large_data, small_data
+
+
+
+def visuliz(istep, data, norm_const, scale):
+  
+    data_2d = norm_const*data[:,:,0]
+    plt.figure(1, figsize = (2.6,2.6), dpi=600)
+    plt.rc('font', family='Helvetica', size=3)
+    plt.imshow(data_2d, origin='none', aspect=1,
+               cmap='coolwarm',vmin=-scale,vmax=scale)
+    plt.colorbar()
     plt.tight_layout()
-    mean = np.around(np.mean(np_data), decimals=2)
-    plt.axvline(x=np.mean(np_data),color='r')
+    plt.axis('off')
+    plt.savefig('vis.pdf', bbox_inches='tight')
+    plt.clf()
+    
+
+
+def hist(data, norm_const, xlabel):
+    plt.clf()
+    data = data.reshape(data.shape[0]*data.shape[1]*data.shape[2])
+    data = norm_const*data
+
+    plt.hist(data, bins=500, density='True',histtype='step', log='True')  
+    plt.ylim((1E-8, 2*10))
+    plt.xlim((-1000, 1000))
+    plt.xlabel(xlabel, fontsize=10, color='black', fontname="Helvetica")
+
+    fig = plt.gcf()
+    fig.set_size_inches(2.6, 2.6)
+    plt.tight_layout()
+    mean = np.around(np.mean(data), decimals=2)
+    plt.axvline(x=np.mean(data),color='r')
     ax = fig.add_subplot(111)
     ax.annotate('mean\n='+str(mean), xy=(mean, 1), xytext=(mean+20, 0.5),
             arrowprops=dict(arrowstyle="->"))
 
-    titlelist = ['diff',variable,'.eps']
+    delimiter = ''
+    titlelist = ['pdf.eps']
     title=delimiter.join(titlelist)
     plt.savefig(title, format='eps', dpi=600)
     plt.show()
+
+
+
+ldata, sdata = init(istep)
+hist(sdata, norm_const, small_label)
+visuliz(istep, sdata, norm_const, 100)
+
+
+hist(ldata, norm_const, large_label)
+visuliz(istep, ldata, norm_const, 20)
+
+
 h5file.close()
